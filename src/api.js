552625c523669e454231fcfd8c5bef59a2ab56ae
Prateek2392001 <103823442+Prateek2392001@users.noreply.github.com>
2025-05-03 01:04:10 +0530

    change base url

--- src/api.js
+++ src/api.js
@@ -1,2 +1,3 @@
-export const api = "https://educationapp-2v1l.onrender.com/api/admin";
+// export const api = "https://educationapp-2v1l.onrender.com/api/admin";
+export const api = "https://pathshala-1wke.onrender.com/api/admin";
 
